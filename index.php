14f6c9bf48ce2e5ea5318108b8cec90ff2236609
ccrs11 <ccrs11@hotmail.com>
2023-05-29 17:23:59 -0500

    feat: :sparkles: output functions in php

--- index.php
+++ index.php
@@ -2,3 +2,13 @@
 /**
  * comments with //
  */
+/**
+ * type of screen prints
+ * output functions
+ * 1 echo 2 print() 3 sprintf()
+ */
+echo '<h1> Hola mundo </h1>';
+$text ='mundo';
+printf("Hola %s", $texto);
+var_dump($text);
+//%s continue with a string value
