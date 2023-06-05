67264e5111614bc24df0dd8a5e45aceaebe6783b
ccrs11 <ccrs11@hotmail.com>
2023-06-05 10:00:52 -0500

    feat: :sparkles: array_map

--- array_methods/index.php
+++ array_methods/index.php
@@ -34,6 +34,21 @@ echo "Odd :\n";
 print_r(array_filter($array2, fn($var) => $var & 1));
 echo '<br>';
 
+/**
+ * * array_map(?callable $callback, array $array, array ...$arrays): array
+ * * returns an array containing the results of aplying the $callback to the $array or $arrays
+ */
+//* one line; the array is determinated by the funtion range [1,2,3,4,5] and the lambda funtion fn($value): int => $value * 2 it's used as callback.
+print_r(array_map(fn($value): int => $value * 2, range(1, 5)));
+echo '<br>';
+
+//* null as a callback to generate an array of arrays
+$a = [1, 2, 3, 4, 5];
+$b = ['one', 'two', 'three', 'four', 'five'];
+$c = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
+$d = array_map(null, $a, $b, $c);
+print_r($d);
+echo '<br>';
 
 
 
