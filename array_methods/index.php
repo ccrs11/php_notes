d8ac1f8d4e39f4e3d3dbdc36e28bcdfc52943955
ccrs11 <ccrs11@hotmail.com>
2023-06-05 08:48:15 -0500

    feat: :sparkles: array_fill

--- array_methods/index.php
+++ array_methods/index.php
@@ -13,3 +13,16 @@ $array1 = array("a"=>"apple", "b"=>"banana");
 $flipResult = array_flip($array1);
 print_r($flipResult);
 //* Array ( [apple] => a [banana] => b )
+
+/**
+ * *  array_fill(int $start_index, int $count, mixed $value): array
+ * *  fill an array from the $start_index with $value, $count times. 
+ * * it is useful also to create an array
+ * * it is can use range(0,X);
+ */
+$fill = array_fill(-2, 4, 'pear');
+print_r($fill);
+
+
+
+
