3f8446afa82d57e40f59551cc83059a393160cee
ccrs11 <ccrs11@hotmail.com>
2023-06-05 11:01:09 -0500

    feat: :sparkles: array_diff

--- array_methods/index.php
+++ array_methods/index.php
@@ -120,6 +120,18 @@ $result = array_intersect($array1, $array2);
 print_r($result);
 echo '<br>';
 
+/**
+ * *  array_diff(array $array, array ...$arrays): array
+ * *  Returns an array containing all the entries from array that are not present in any of the other arrays.
+ */
+$array1 = array("a" => "green", "red", "blue", "red");
+$array2 = array("b" => "green", "yellow", "red");
+$result = array_diff($array1, $array2);
+print_r($result);
+echo '<br>';
+
+
+
 
 
 
